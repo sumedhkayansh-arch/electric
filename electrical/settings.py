ee94a9af1535967f17084f280d84af6f34de4605
sumedhkayansh-arch <sumedhkayansh@gmail.com>
2026-06-04 20:36:21 +0530

    Update settings.py

--- electrical/settings.py
+++ electrical/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-clbr^3vbr3_4ni)_#8%juxxg1$70#9$5@ku+=$6g6kwheuu^kr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://electric-mrlu.onrender.com']
 
 
 # Application definition
